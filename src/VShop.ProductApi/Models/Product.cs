809d057f7a4562b3611ac1e73ae41d87049465dd
Josué Diogo Pedro <pedrojosuediogo@gmail.com>
2024-03-22 08:04:44 -0700

    Add Product model

---
+++ src/VShop.ProductApi/Models/Product.cs
@@ -0,0 +1,11 @@
+﻿namespace VShop.ProductApi.Models;
+
+public class Product
+{
+    public int ProductId { get; set; }
+    public string? Name { get; set; }
+    public decimal Price { get; set; }
+    public string? Description { get; set; }
+    public long Stock { get; set; }
+    public string? ImagemURL { get; set; }
+}
