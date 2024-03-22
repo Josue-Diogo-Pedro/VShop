72c47a5228fdb95f4bb30a9c22843ef1c18a30c5
Josué Diogo Pedro <pedrojosuediogo@gmail.com>
2024-03-22 08:10:52 -0700

    Add context in project

---
+++ src/VShop.ProductApi/Context/ProductApiDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using VShop.ProductApi.Models;
+
+namespace VShop.ProductApi.Context;
+
+public class ProductApiDbContext : DbContext
+{
+    public DbSet<Product>? Products { get; set; }
+    public DbSet<Category>? Categories { get; set; }
+
+    public ProductApiDbContext(DbContextOptions<ProductApiDbContext> options) : base(options) { }
+}
