45df15d5ec50eba17b80b38a5496634fb029dc8d
Josué Diogo Pedro <pedrojosuediogo@gmail.com>
2024-03-22 13:18:02 -0700

    Create ProductRepository

---
+++ src/VShop.ProductApi/Repositories/ProductRepository.cs
@@ -0,0 +1,32 @@
+﻿using VShop.ProductApi.Models;
+
+namespace VShop.ProductApi.Repositories;
+
+public class ProductRepository : IProductRepository
+{
+
+    public Task<Product> Create(Product product)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Product> Delete(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<IEnumerable<Product>> GetAll()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Product> GetById(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Product> Update(Product product)
+    {
+        throw new NotImplementedException();
+    }
+}
