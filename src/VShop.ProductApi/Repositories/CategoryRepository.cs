26afb287a05b8f49dcf91a7ab1db382ed0f5fdd9
Josué Diogo Pedro <pedrojosuediogo@gmail.com>
2024-03-22 10:29:12 -0700

    Creating implementation of CategoryRepository

---
+++ src/VShop.ProductApi/Repositories/CategoryRepository.cs
@@ -0,0 +1,36 @@
+﻿using VShop.ProductApi.Models;
+
+namespace VShop.ProductApi.Repositories;
+
+public class CategoryRepository : ICategoryRepository
+{
+    public Task<Category> Create(Category category)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Category> Delete(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<IEnumerable<Category>> GetAll()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Category> GetById(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<IEnumerable<Category>> GetCategoriesProducts()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Category> Update(Category category)
+    {
+        throw new NotImplementedException();
+    }
+}
