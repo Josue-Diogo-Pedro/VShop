bcfa0b28a0ca6921d713928d55481d7167b107fc
Josué Diogo Pedro <pedrojosuediogo@gmail.com>
2024-03-22 09:50:36 -0700

    Add ProductDTO

---
+++ src/VShop.ProductApi/DTOs/ProductDTO.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+using VShop.ProductApi.Models;
+
+namespace VShop.ProductApi.DTOs;
+
+public class ProductDTO
+{
+    public int ProductId { get; set; }
+
+    [Required(ErrorMessage = "The Name is required")]
+    [MinLength(3)]
+    [MaxLength(100)]
+    public string? Name { get; set; }
+
+    [Required(ErrorMessage = "The Price is required")]
+    public decimal Price { get; set; }
+
+    [Required(ErrorMessage = "The Description is required")]
+    [MinLength(5)]
+    [MaxLength(200)]
+    public string? Description { get; set; }
+
+    [Required(ErrorMessage = "The Stock is required")]
+    [Range(1, 9999)]
+    public long Stock { get; set; }
+
+    public string? ImagemURL { get; set; }
+
+    //EF relations
+    public Category? Category { get; set; }
+    public int CategoryId { get; set; }
+}
