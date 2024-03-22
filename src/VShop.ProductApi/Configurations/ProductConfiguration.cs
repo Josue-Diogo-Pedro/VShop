c5f6ace6b8564c59911f89143cdbfa40d3e7d46e
Josué Diogo Pedro <pedrojosuediogo@gmail.com>
2024-03-22 08:52:06 -0700

    Add configurations for Product with Fluent API

---
+++ src/VShop.ProductApi/Configurations/ProductConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using VShop.ProductApi.Models;
+
+namespace VShop.ProductApi.Configurations;
+
+public class ProductConfiguration : IEntityTypeConfiguration<Product>
+{
+    public void Configure(EntityTypeBuilder<Product> builder)
+    {
+        builder.ToTable("Products");
+        builder.HasKey(id => id.ProductId);
+
+        builder.Property(p => p.Name).HasMaxLength(100).IsRequired();
+        builder.Property(p => p.Description).HasMaxLength(255).IsRequired();
+        builder.Property(p => p.ImagemURL).HasMaxLength(255).IsRequired();
+        builder.Property(p => p.Price).HasPrecision(10, 2);
+    }
+}
