d3bf679cdd4686d460d371c30748dd894f6be4c7
Josué Diogo Pedro <pedrojosuediogo@gmail.com>
2024-03-22 08:15:46 -0700

    Register context in programs and add connectionStrings in appsettings

--- src/VShop.ProductApi/Program.cs
+++ src/VShop.ProductApi/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using VShop.ProductApi.Context;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,9 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<ProductApiDbContext>(options =>
+                              options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
