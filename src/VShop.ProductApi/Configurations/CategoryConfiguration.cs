d07b51d656c5458e11811f3c80175be895ea0182
Josué Diogo Pedro <pedrojosuediogo@gmail.com>
2024-03-22 08:58:03 -0700

    Add configurations for Category, Fluent API and initial data

---
+++ src/VShop.ProductApi/Configurations/CategoryConfiguration.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using VShop.ProductApi.Models;
+
+namespace VShop.ProductApi.Configurations;
+
+public class CategoryConfiguration : IEntityTypeConfiguration<Category>
+{
+    public void Configure(EntityTypeBuilder<Category> builder)
+    {
+        builder.ToTable("Categories");
+        builder.HasKey(id => id.CategoryId);
+
+        builder.Property(p => p.Name).HasMaxLength(100).IsRequired();
+        builder.HasMany(p => p.Products)
+               .WithOne(p => p.Category)
+               .IsRequired()
+               .OnDelete(DeleteBehavior.Cascade);
+
+        builder.HasData(
+            new Category
+            {
+                CategoryId = 1,
+                Name = "Material Escolar"
+            },
+            new Category
+            {
+                CategoryId = 2,
+                Name = "Acessórios"
+            }
+        );
+    }
+}
