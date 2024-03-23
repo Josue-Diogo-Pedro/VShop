600a7bbaaf5d2b17be58004469ea0d6a0bec8d55
Josué Diogo Pedro <pedrojosuediogo@gmail.com>
2024-03-23 06:28:06 -0700

    VShop.Web - create variables and constructor

--- src/VShop.Web/Services/ProductService.cs
+++ src/VShop.Web/Services/ProductService.cs
@@ -1,10 +1,23 @@
-﻿using VShop.Web.Models;
+﻿using System.Text.Json;
+using VShop.Web.Models;
 using VShop.Web.Services.Contracts;
 
 namespace VShop.Web.Services;
 
 public class ProductService : IProductService
 {
+    private readonly IHttpClientFactory _clientFactory;
+    private const string apiEndpoint = "/api/products/";
+    private readonly JsonSerializerOptions _options;
+    private IEnumerable<ProductViewModel> _productsVM;
+    private ProductViewModel _productVM;
+
+    public ProductService(IHttpClientFactory clientFactory)
+    {
+        _options = new JsonSerializerOptions { PropertyNameCaseInsensitive = true };
+        _clientFactory = clientFactory;
+    }
+
     public Task<IEnumerable<ProductViewModel>> GetAllProducts()
     {
         throw new NotImplementedException();
