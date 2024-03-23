1115e639eb33c6e15cb191ed0cc72ad83e7eda65
Josué Diogo Pedro <pedrojosuediogo@gmail.com>
2024-03-23 06:24:29 -0700

    VShop.Web - begin to implement ProductService and register httpclient in program and appsetting.json for ProductApi

--- src/VShop.Web/Program.cs
+++ src/VShop.Web/Program.cs
@@ -3,6 +3,11 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+builder.Services.AddHttpClient("ProductApi", c =>
+{
+    c.BaseAddress = new Uri(builder.Configuration["ServiceUri:ProductApi"]);
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
---
+++ src/VShop.Web/Services/ProductService.cs
@@ -0,0 +1,32 @@
+﻿using VShop.Web.Models;
+using VShop.Web.Services.Contracts;
+
+namespace VShop.Web.Services;
+
+public class ProductService : IProductService
+{
+    public Task<IEnumerable<ProductViewModel>> GetAllProducts()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<ProductViewModel> FindProductById(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<ProductViewModel> CreateProduct(ProductViewModel productVM)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<ProductViewModel> UpdateProduct(ProductViewModel productVM)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<bool> DeleteProductById(int id)
+    {
+        throw new NotImplementedException();
+    }
+}
