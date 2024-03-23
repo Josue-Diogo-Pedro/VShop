71f7460b192c46e0954d84600f1ac86e09972ecc
Josué Diogo Pedro <pedrojosuediogo@gmail.com>
2024-03-23 09:23:38 -0700

    VShop.Web - create service for get all categories

--- src/VShop.Web/Services/CategoryService.cs
+++ src/VShop.Web/Services/CategoryService.cs
@@ -1,6 +1,35 @@
-﻿namespace VShop.Web.Services;
+﻿using System.Text.Json;
+using VShop.Web.Models;
+using VShop.Web.Services.Contracts;
 
-public class CategoryService
+namespace VShop.Web.Services;
+
+public class CategoryService : ICategoryService
 {
+    private readonly IHttpClientFactory _clientFactory;
+    private readonly JsonSerializerOptions _options;
+    private const string apiEndpoint = "/api/categories/";
 
+    public CategoryService(IHttpClientFactory clientFactory)
+    {
+        _options = new JsonSerializerOptions { PropertyNameCaseInsensitive = true };
+        _clientFactory = clientFactory;
+    }
+
+    public async Task<IEnumerable<CategoryViewModel>> GetAllCategories()
+    {
+        var client = _clientFactory.CreateClient("ProductApi");
+        IEnumerable<CategoryViewModel> categories;
+
+        using (var response = await client.GetAsync(apiEndpoint))
+        {
+            if (response.IsSuccessStatusCode)
+            {
+                var apiResponse = await response.Content.ReadAsStreamAsync();
+                categories = await JsonSerializer.DeserializeAsync<IEnumerable<CategoryViewModel>>(apiResponse, _options);
+            }
+            else return null;
+        }
+        return categories;
+    }
 }
