01565ebb60fd848c0b54f2653609433d76b78b54
Josué Diogo Pedro <pedrojosuediogo@gmail.com>
2024-03-22 23:20:40 -0700

    Begin to implement CategoryService

---
+++ src/VShop.ProductApi/Services/_Category/CategoryService.cs
@@ -0,0 +1,36 @@
+﻿using VShop.ProductApi.DTOs;
+
+namespace VShop.ProductApi.Services._Category;
+
+public class CategoryService : ICategoryService
+{
+
+
+    public Task<IEnumerable<CategoryDTO>> GetCategories()
+    {
+        throw new NotImplementedException();
+    }
+    public Task<IEnumerable<CategoryDTO>> GetCategoriesProducts()
+    {
+        throw new NotImplementedException();
+    }
+    public Task<CategoryDTO> GetCategoryById(int id)
+    {
+        throw new NotImplementedException();
+    }
+    public Task AddCategory(CategoryDTO categoryDTO)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task UpdateCategory(CategoryDTO categoryDTO)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task RemoveCategory(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+}
