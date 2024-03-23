85cd06c9dc14d50209097d6903e3ffef9d6be3df
Josué Diogo Pedro <pedrojosuediogo@gmail.com>
2024-03-22 23:17:23 -0700

    Add ICategoryService

---
+++ src/VShop.ProductApi/Services/_Category/ICategoryService.cs
@@ -0,0 +1,13 @@
+﻿using VShop.ProductApi.DTOs;
+
+namespace VShop.ProductApi.Services._Category;
+
+public interface ICategoryService
+{
+    Task<IEnumerable<CategoryDTO>> GetCategories();
+    Task<IEnumerable<CategoryDTO>> GetCategoriesProducts();
+    Task<CategoryDTO> GetCategoryById(int id);
+    Task AddCategory(CategoryDTO categoryDTO);
+    Task UpdateCategory(CategoryDTO categoryDTO);
+    Task RemoveCategory(int id);
+}
