b576c5665d9ee1e526f7c5d6f7c882e4a74d29db
Josué Diogo Pedro <pedrojosuediogo@gmail.com>
2024-03-23 08:55:42 -0700

    VShop.Web - Create endpoint Index

--- src/VShop.Web/Controllers/ProductsController.cs
+++ src/VShop.Web/Controllers/ProductsController.cs
@@ -1,11 +1,20 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using VShop.Web.Models;
+using VShop.Web.Services.Contracts;
 
 namespace VShop.Web.Controllers;
 
 public class ProductsController : Controller
 {
-    public IActionResult Index()
+    private readonly IProductService _productService;
+
+    public ProductsController(IProductService productService) => _productService = productService;
+
+    public async Task<ActionResult<IEnumerable<ProductViewModel>>> Index()
     {
-        return View();
+        var result = await _productService.GetAllProducts();
+        if (result is null) return View("Error");
+
+        return View(result);
     }
 }
