c7b4bfbb6ef312c722bad822ad9743f0969b351b
Josué Diogo Pedro <pedrojosuediogo@gmail.com>
2024-03-22 23:33:05 -0700

    Add IProductService

---
+++ src/VShop.ProductApi/Services/_Product/IProductService.cs
@@ -0,0 +1,12 @@
+﻿using VShop.ProductApi.DTOs;
+
+namespace VShop.ProductApi.Services._Product;
+
+public interface IProductService
+{
+    Task<IEnumerable<ProductDTO>> GetProducts();
+    Task<ProductDTO> GetProductById(int id);
+    Task AddProduct(ProductDTO product);
+    Task UpdateProduct(ProductDTO product);
+    Task RemoveProduct(int id);
+}
