d7690b3297a1fda4c4803bd624d8f53aa4e057e4
Josué Diogo Pedro <pedrojosuediogo@gmail.com>
2024-03-23 06:10:07 -0700

    VShop.Web - Create ProductViewModel

---
+++ src/VShop.Web/Models/ProductViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace VShop.Web.Models;
+
+public class ProductViewModel
+{
+    public int ProductId { get; set; }
+    public string? Name { get; set; }
+    public decimal Price { get; set; }
+    public string? Description { get; set; }
+    public long Stock { get; set; }
+    public string? CategoryName { get; set; }
+    public string? ImagemURL { get; set; }
+    public int CategoryId { get; set; }
+}
