477b61b7a53e820391897890dfa4a08e1c9030e8
Josué Diogo Pedro <pedrojosuediogo@gmail.com>
2024-03-22 23:46:23 -0700

    Finish to implement ProductService

--- src/VShop.ProductApi/Services/_Product/ProductService.cs
+++ src/VShop.ProductApi/Services/_Product/ProductService.cs
@@ -1,31 +1,50 @@
-﻿using VShop.ProductApi.DTOs;
+﻿using AutoMapper;
+using VShop.ProductApi.DTOs;
+using VShop.ProductApi.Models;
+using VShop.ProductApi.Repositories;
 
 namespace VShop.ProductApi.Services._Product;
 
 public class ProductService : IProductService
 {
-    public Task AddProduct(ProductDTO product)
+    private readonly IProductRepository _productRepository;
+    private readonly IMapper _mapper;
+
+    public ProductService(IProductRepository productRepository, IMapper mapper)
     {
-        throw new NotImplementedException();
+        _productRepository = productRepository;
+        _mapper = mapper;
     }
 
-    public Task<ProductDTO> GetProductById(int id)
+    public async Task<IEnumerable<ProductDTO>> GetProducts()
     {
-        throw new NotImplementedException();
+        var products = await _productRepository.GetAll();
+        return _mapper.Map<IEnumerable<ProductDTO>>(products);
     }
 
-    public Task<IEnumerable<ProductDTO>> GetProducts()
+    public async Task<ProductDTO> GetProductById(int id)
     {
-        throw new NotImplementedException();
+        var product = await _productRepository.GetById(id);
+        return _mapper.Map<ProductDTO>(product);
     }
 
-    public Task RemoveProduct(int id)
+    public async Task AddProduct(ProductDTO product)
     {
-        throw new NotImplementedException();
+        var productResult = _mapper.Map<Product>(product);
+        await _productRepository.Create(productResult);
+        product.ProductId = productResult.ProductId;
     }
 
-    public Task UpdateProduct(ProductDTO product)
+    public async Task UpdateProduct(ProductDTO product)
     {
-        throw new NotImplementedException();
+        var productResult = _mapper.Map<Product>(product);
+        await _productRepository.Update(productResult);
     }
+
+    public async Task RemoveProduct(int id)
+    {
+        var product = await _productRepository.GetById(id);
+        await _productRepository.Delete(product.ProductId);
+    }
+
 }
