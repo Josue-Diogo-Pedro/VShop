056a1f3ca10d378ac8bd0253c0ff7f51d652c049
Josué Diogo Pedro <pedrojosuediogo@gmail.com>
2024-03-22 23:29:47 -0700

    Finish to implement CategoryService

--- src/VShop.ProductApi/Services/_Category/CategoryService.cs
+++ src/VShop.ProductApi/Services/_Category/CategoryService.cs
@@ -1,36 +1,56 @@
-﻿using VShop.ProductApi.DTOs;
+﻿using AutoMapper;
+using VShop.ProductApi.DTOs;
+using VShop.ProductApi.Models;
+using VShop.ProductApi.Repositories;
 
 namespace VShop.ProductApi.Services._Category;
 
 public class CategoryService : ICategoryService
 {
+    private readonly ICategoryRepository _categoryRepository;
+    private readonly IMapper _mapper;
 
-
-    public Task<IEnumerable<CategoryDTO>> GetCategories()
+    public CategoryService(ICategoryRepository categoryRepository, IMapper mapper)
     {
-        throw new NotImplementedException();
-    }
-    public Task<IEnumerable<CategoryDTO>> GetCategoriesProducts()
-    {
-        throw new NotImplementedException();
-    }
-    public Task<CategoryDTO> GetCategoryById(int id)
-    {
-        throw new NotImplementedException();
-    }
-    public Task AddCategory(CategoryDTO categoryDTO)
-    {
-        throw new NotImplementedException();
+        _categoryRepository = categoryRepository;
+        _mapper = mapper;
     }
 
-    public Task UpdateCategory(CategoryDTO categoryDTO)
+    public async Task<IEnumerable<CategoryDTO>> GetCategories()
     {
-        throw new NotImplementedException();
+        var categories = await _categoryRepository.GetAll();
+        return _mapper.Map<IEnumerable<CategoryDTO>>(categories);
     }
 
-    public Task RemoveCategory(int id)
+    public async Task<IEnumerable<CategoryDTO>> GetCategoriesProducts()
     {
-        throw new NotImplementedException();
+        var categoriesProducts = await _categoryRepository.GetCategoriesProducts();
+        return _mapper.Map<IEnumerable<CategoryDTO>>(categoriesProducts);
+    }
+
+    public async Task<CategoryDTO> GetCategoryById(int id)
+    {
+        var category = await _categoryRepository.GetById(id);
+        return _mapper.Map<CategoryDTO>(category);
+    }
+
+    public async Task AddCategory(CategoryDTO categoryDTO)
+    {
+        var category = _mapper.Map<Category>(categoryDTO);
+        await _categoryRepository.Create(category);
+        categoryDTO.CategoryId = category.CategoryId;
+    }
+
+    public async Task UpdateCategory(CategoryDTO categoryDTO)
+    {
+        var category = _mapper.Map<Category>(categoryDTO);
+        await _categoryRepository.Update(category);
+    }
+
+    public async Task RemoveCategory(int id)
+    {
+        var category = await _categoryRepository.GetById(id);
+        await _categoryRepository.Delete(category.CategoryId);
     }
 
 }
