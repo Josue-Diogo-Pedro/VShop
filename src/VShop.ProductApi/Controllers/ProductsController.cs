f5ed9a0fa9a686572a20cd347e74892e4df238f4
Josué Diogo Pedro <pedrojosuediogo@gmail.com>
2024-03-23 03:23:13 -0700

    Begin to implement ProductsController

---
+++ src/VShop.ProductApi/Controllers/ProductsController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using VShop.ProductApi.Services._Product;
+
+namespace VShop.ProductApi.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class ProductsController : ControllerBase
+{
+    private readonly IProductService _productService;
+
+	public ProductsController(IProductService productService) => _productService = productService;
+}
