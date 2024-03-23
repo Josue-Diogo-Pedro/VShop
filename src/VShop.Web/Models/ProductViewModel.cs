7627e0b2b45915b61b84668e86d9b80d4bb5af14
Josué Diogo Pedro <pedrojosuediogo@gmail.com>
2024-03-23 09:05:34 -0700

    VShop.Web - add data anotations in productViewModel

--- src/VShop.Web/Models/ProductViewModel.cs
+++ src/VShop.Web/Models/ProductViewModel.cs
@@ -1,16 +1,32 @@
 ﻿using System.ComponentModel.DataAnnotations;
-using System.Text.Json.Serialization;
 
 namespace VShop.Web.Models;
 
 public class ProductViewModel
 {
     public int ProductId { get; set; }
+
+    [Required]
     public string? Name { get; set; }
+
+    [Range(1, 9999)]
+    [Required]
     public decimal Price { get; set; }
+
+    [Required]
     public string? Description { get; set; }
+
+    [Range(1, 9999)]
+    [Required]
     public long Stock { get; set; }
+
+    [Display(Name = "Category Name")]
     public string? CategoryName { get; set; }
+
+    [Required]
+    [Display(Name = "Image URL")]
     public string? ImagemURL { get; set; }
+
+    [Display(Name = "Category")]
     public int CategoryId { get; set; }
 }
