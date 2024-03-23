6e13e37fef7941eec086478fe7ed44042f66d257
Josué Diogo Pedro <pedrojosuediogo@gmail.com>
2024-03-22 23:37:11 -0700

    Begin to create ProductService

---
+++ src/VShop.ProductApi/Services/_Product/ProductService.cs
@@ -0,0 +1,31 @@
+﻿using VShop.ProductApi.DTOs;
+
+namespace VShop.ProductApi.Services._Product;
+
+public class ProductService : IProductService
+{
+    public Task AddProduct(ProductDTO product)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<ProductDTO> GetProductById(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<IEnumerable<ProductDTO>> GetProducts()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task RemoveProduct(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task UpdateProduct(ProductDTO product)
+    {
+        throw new NotImplementedException();
+    }
+}
