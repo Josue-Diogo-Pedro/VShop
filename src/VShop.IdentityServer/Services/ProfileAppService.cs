19bdcad42dd3744e22b5c4017d1f33ad4bb033ef
Josué Diogo Pedro <pedrojosuediogo@gmail.com>
2024-03-24 14:44:39 -0700

    VShop.IdentityServer - create class to implement iprofileservice to return profile in token

---
+++ src/VShop.IdentityServer/Services/ProfileAppService.cs
@@ -0,0 +1,17 @@
+﻿using Duende.IdentityServer.Models;
+using Duende.IdentityServer.Services;
+
+namespace VShop.IdentityServer.Services;
+
+public class ProfileAppService : IProfileService
+{
+    public Task GetProfileDataAsync(ProfileDataRequestContext context)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task IsActiveAsync(IsActiveContext context)
+    {
+        throw new NotImplementedException();
+    }
+}
