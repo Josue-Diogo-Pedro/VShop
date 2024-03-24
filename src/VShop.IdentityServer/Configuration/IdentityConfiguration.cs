e925740fa97a1269171da24d6cd47a93d669d710
Josué Diogo Pedro <pedrojosuediogo@gmail.com>
2024-03-24 07:04:15 -0700

    VShop.IdentityServer - create configuration class for identity and begin to implement

---
+++ src/VShop.IdentityServer/Configuration/IdentityConfiguration.cs
@@ -0,0 +1,25 @@
+﻿using Duende.IdentityServer.Models;
+
+namespace VShop.IdentityServer.Configuration;
+
+public class IdentityConfiguration
+{
+    public const string Admin = "Admin";
+    public const string Client = "Client";
+
+    public static IEnumerable<IdentityResource> IdentityResources => new List<IdentityResource>
+    {
+        new IdentityResources.OpenId(),
+        new IdentityResources.Email(),
+        new IdentityResources.Profile()
+    };
+
+    public static IEnumerable<ApiScope> ApiScopes => new List<ApiScope>
+    {
+        //vshop is web application that will access IdentityServer to obtain the token
+        new ApiScope("vshop", "VShop Server"),
+        new ApiScope("read", "Read data"),
+        new ApiScope("write", "Write data"),
+        new ApiScope("delete", "Delete data")
+    };
+}
