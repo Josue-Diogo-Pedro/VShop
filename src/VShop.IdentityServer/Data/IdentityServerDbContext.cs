c2bd8be29a54380ff1ef1b2d753d2eca82c08436
Josué Diogo Pedro <pedrojosuediogo@gmail.com>
2024-03-24 06:52:06 -0700

    VShop.IdentityServer - add context and connectionString

---
+++ src/VShop.IdentityServer/Data/IdentityServerDbContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace VShop.IdentityServer.Data;
+
+public class IdentityServerDbContext : IdentityDbContext<ApplicationUser>
+{
+	public IdentityServerDbContext(DbContextOptions<IdentityServerDbContext> options) : base(options) { }
+}
