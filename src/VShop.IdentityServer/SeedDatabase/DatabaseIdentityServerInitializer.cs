695d10c1a662ef1b26b164fb3f0efa1267eab136
Josué Diogo Pedro <pedrojosuediogo@gmail.com>
2024-03-24 10:55:59 -0700

    VShop.IdentityServer - add implementation of initializeSeedRoles method

--- src/VShop.IdentityServer/SeedDatabase/DatabaseIdentityServerInitializer.cs
+++ src/VShop.IdentityServer/SeedDatabase/DatabaseIdentityServerInitializer.cs
@@ -1,11 +1,40 @@
-﻿namespace VShop.IdentityServer.SeedDatabase;
+﻿using Microsoft.AspNetCore.Identity;
+using VShop.IdentityServer.Configuration;
+using VShop.IdentityServer.Data;
+
+namespace VShop.IdentityServer.SeedDatabase;
 
 public class DatabaseIdentityServerInitializer : IDatabaseSeedInitializer
 {
+    private readonly UserManager<ApplicationUser> _userManager;
+    private readonly RoleManager<IdentityRole> _roleManager;
 
-    public void InitializeSeedRoles()
+    public DatabaseIdentityServerInitializer(UserManager<ApplicationUser> userManager, RoleManager<IdentityRole> roleManager)
     {
-        throw new NotImplementedException();
+        _userManager = userManager;
+        _roleManager = roleManager;
+    }
+
+    public async void InitializeSeedRoles()
+    {
+        //If Admin profile does not exist, then create profile
+        if(!await _roleManager.RoleExistsAsync(IdentityConfiguration.Admin))
+        {
+            //Create Admin profile
+            IdentityRole roleAdmin = new();
+            roleAdmin.Name = IdentityConfiguration.Admin;
+            roleAdmin.NormalizedName = IdentityConfiguration.Admin.ToUpper();
+            await _roleManager.CreateAsync(roleAdmin);
+        }
+
+        //If Client profile doesn't exist, then create profile
+        if(!await _roleManager.RoleExistsAsync(IdentityConfiguration.Client))
+        {
+            IdentityRole roleClient = new();
+            roleClient.Name = IdentityConfiguration.Client;
+            roleClient.NormalizedName = IdentityConfiguration.Client.ToUpper();
+            await _roleManager.CreateAsync(roleClient);
+        }
     }
 
     public void InitializeSeedUsers()
