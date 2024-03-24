4120102ba6751b20f79042898f10b2ba166970c4
Josué Diogo Pedro <pedrojosuediogo@gmail.com>
2024-03-24 14:46:22 -0700

    VShop.IdentityServer - register usermanager, siginmanager and rolemanager in constructor

--- src/VShop.IdentityServer/Services/ProfileAppService.cs
+++ src/VShop.IdentityServer/Services/ProfileAppService.cs
@@ -1,10 +1,25 @@
 ﻿using Duende.IdentityServer.Models;
 using Duende.IdentityServer.Services;
+using Microsoft.AspNetCore.Identity;
+using VShop.IdentityServer.Data;
 
 namespace VShop.IdentityServer.Services;
 
 public class ProfileAppService : IProfileService
 {
+    private readonly UserManager<ApplicationUser> _userManager;
+    private readonly SignInManager<ApplicationUser> _signInManager;
+    private readonly RoleManager<IdentityRole> _roleManager;
+
+    public ProfileAppService(UserManager<ApplicationUser> userManager, 
+        SignInManager<ApplicationUser> signInManager, 
+        RoleManager<IdentityRole> roleManager)
+    {
+        _userManager = userManager;
+        _signInManager = signInManager;
+        _roleManager = roleManager;
+    }
+
     public Task GetProfileDataAsync(ProfileDataRequestContext context)
     {
         throw new NotImplementedException();
