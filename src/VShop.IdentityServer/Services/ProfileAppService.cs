8e5e3886c7d9819601763910db84b40e57137fc2
Josué Diogo Pedro <pedrojosuediogo@gmail.com>
2024-04-01 04:22:58 -0700

    VShop.IdentityServer - Update script and GetProfileDataAsync

--- src/VShop.IdentityServer/Services/ProfileAppService.cs
+++ src/VShop.IdentityServer/Services/ProfileAppService.cs
@@ -1,6 +1,9 @@
-﻿using Duende.IdentityServer.Models;
+﻿using Duende.IdentityServer.Extensions;
+using Duende.IdentityServer.Models;
 using Duende.IdentityServer.Services;
+using IdentityModel;
 using Microsoft.AspNetCore.Identity;
+using System.Security.Claims;
 using VShop.IdentityServer.Data;
 
 namespace VShop.IdentityServer.Services;
@@ -8,21 +11,65 @@ namespace VShop.IdentityServer.Services;
 public class ProfileAppService : IProfileService
 {
     private readonly UserManager<ApplicationUser> _userManager;
-    private readonly SignInManager<ApplicationUser> _signInManager;
     private readonly RoleManager<IdentityRole> _roleManager;
+    private readonly IUserClaimsPrincipalFactory<ApplicationUser> _userClaimsPrincipalFactory;
 
-    public ProfileAppService(UserManager<ApplicationUser> userManager, 
-        SignInManager<ApplicationUser> signInManager, 
+    public ProfileAppService(UserManager<ApplicationUser> userManager,
+        IUserClaimsPrincipalFactory<ApplicationUser> userClaimsPrincipalFactory, 
         RoleManager<IdentityRole> roleManager)
     {
         _userManager = userManager;
-        _signInManager = signInManager;
+        _userClaimsPrincipalFactory = userClaimsPrincipalFactory;
         _roleManager = roleManager;
     }
 
-    public Task GetProfileDataAsync(ProfileDataRequestContext context)
+    public async Task GetProfileDataAsync(ProfileDataRequestContext context)
     {
-        throw new NotImplementedException();
+        //user id in IS
+        var id = context.Subject.GetSubjectId();
+
+        //Find user by id
+        ApplicationUser user = await _userManager.FindByIdAsync(id);
+
+        //Create principal claims to user
+        ClaimsPrincipal userClaims = await _userClaimsPrincipalFactory.CreateAsync(user);
+
+        //Define collection of claims to user and include surname and user name
+        List<Claim> claims = userClaims.Claims.ToList();
+        claims.Add(new Claim(JwtClaimTypes.FamilyName, user.LastName));
+        claims.Add(new Claim(JwtClaimTypes.GivenName, user.FirstName));
+
+        //Se o userManager do identity suportar role
+        if (_userManager.SupportsUserRole)
+        {
+            //Obtain list of role names to user
+            IList<string> roles = await _userManager.GetRolesAsync(user);
+
+            //navigate to list
+            foreach (var role in roles)
+            {
+                //add role into claim
+                claims.Add(new Claim(JwtClaimTypes.Role, role));
+
+                //Se _rolemanager suportar claims para roles
+                if (_roleManager.SupportsRoleClaims)
+                {
+                    //find user
+                    IdentityRole identityRole = await _roleManager.FindByNameAsync(role);
+
+                    //include profile
+                    if(identityRole is not null)
+                    {
+                        //include the claims associate by role
+                        claims.AddRange(await _roleManager.GetClaimsAsync(identityRole));
+                    }
+                }
+            }
+
+        }
+
+        //return the claims of context
+        context.IssuedClaims = claims;
     }
 
     public Task IsActiveAsync(IsActiveContext context)
