40f605461fc813999cd1d23b9c121b67fa907988
Josué Diogo Pedro <pedrojosuediogo@gmail.com>
2024-04-06 07:05:45 -0700

    VShop.CartApi - create CartHeader

---
+++ src/VShop.CartApi/Models/CartHeader.cs
@@ -0,0 +1,8 @@
+﻿namespace VShop.CartApi.Models;
+
+public class CartHeader
+{
+    public int CartHeaderId { get; set; }
+    public string UserId { get; set; } = string.Empty;
+    public string CouponCode { get; set; } = string.Empty; 
+}
