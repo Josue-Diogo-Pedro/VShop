e761db75d3b3b0f4284d4d3c78be0393578fa3a9
Josué Diogo Pedro <pedrojosuediogo@gmail.com>
2024-04-06 08:14:44 -0700

    VShop.CartApi - create Cart and CartItem

---
+++ src/VShop.CartApi/Models/Cart.cs
@@ -0,0 +1,7 @@
+﻿namespace VShop.CartApi.Models;
+
+public class Cart
+{
+    public CartHeader CartHeader { get; set; } = new();
+    public IEnumerable<CartItem> CartItems { get; set; } = new List<CartItem>();
+}
---
+++ src/VShop.CartApi/Models/CartItem.cs
@@ -0,0 +1,11 @@
+﻿namespace VShop.CartApi.Models;
+
+public class CartItem
+{
+    public int CartItemId { get; set; }
+    public int Quantity { get; set; }
+    public int ProductId { get; set; }
+    public int CartHeaderId { get; set; }
+    public Product Product { get; set; } = new();
+    public CartHeader CartHeader { get; set; } = new();
+}
