6bd1f4c9266f5b620617c38ca4988af499472b0a
Josué Diogo Pedro <pedrojosuediogo@gmail.com>
2024-04-07 14:46:38 -0700

    VShop.CartApi - add configuration for Product

---
+++ src/VShop.CartApi/Configurations/ProductConfiguration.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using VShop.CartApi.Models;
+
+namespace VShop.CartApi.Configurations;
+
+public class ProductConfiguration : IEntityTypeConfiguration<Product>
+{
+    public void Configure(EntityTypeBuilder<Product> builder)
+    {
+        builder.ToTable("Products");
+        builder.HasKey(x => x.ProductId);
+
+        builder.Property(p => p.ProductId).ValueGeneratedNever();
+        builder.Property(p => p.Name).HasMaxLength(100).IsRequired();
+        builder.Property(p => p.ImageURL).HasMaxLength(255).IsRequired();
+        builder.Property(p => p.CategoryName).HasMaxLength(100).IsRequired();
+        builder.Property(p => p.Price).HasPrecision(12, 2);
+
+    }
+}
