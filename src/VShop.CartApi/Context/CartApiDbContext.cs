0c690089fb0d53cfb6d3a2f540c13a440710854f
Josué Diogo Pedro <pedrojosuediogo@gmail.com>
2024-04-07 14:40:32 -0700

    VShop.CartApi - add context

---
+++ src/VShop.CartApi/Context/CartApiDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using VShop.CartApi.Models;
+
+namespace VShop.CartApi.Context;
+
+public class CartApiDbContext : DbContext
+{
+    public DbSet<Product>? Products { get; set; }
+    public DbSet<CartHeader> CartHeader { get; set; }
+    public DbSet<Cart> Carts { get; set; }
+
+    public CartApiDbContext(DbContextOptions<CartApiDbContext> options): base(options) { }
+}
