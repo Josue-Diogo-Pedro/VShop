c03ca672526d8c041067ccaa79922af44d14ef1b
Josué Diogo Pedro <pedrojosuediogo@gmail.com>
2024-04-07 14:50:13 -0700

    VShop.CartApi - add configuration for CartHeader

---
+++ src/VShop.CartApi/Configurations/CartHeaderConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using VShop.CartApi.Models;
+
+namespace VShop.CartApi.Configurations;
+
+public class CartHeaderConfiguration : IEntityTypeConfiguration<CartHeader>
+{
+    public void Configure(EntityTypeBuilder<CartHeader> builder)
+    {
+        builder.ToTable("CartHeaders");
+        builder.HasKey(k => k.CartHeaderId);
+
+        builder.Property(p => p.UserId).HasMaxLength(255).IsRequired();
+        builder.Property(p => p.CouponCode).HasMaxLength(100);
+    }
+}
