3b3877ae02c1fdc823677c75ffc0b8d51d3609a3
Josué Diogo Pedro <pedrojosuediogo@gmail.com>
2024-04-07 15:02:54 -0700

    VShop.CartApi - add cors

--- src/VShop.CartApi/Program.cs
+++ src/VShop.CartApi/Program.cs
@@ -47,6 +47,13 @@ builder.Services.AddDbContext<CartApiDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("CorsPolicy", builder => builder.AllowAnyOrigin()
+                                                      .AllowAnyMethod()
+                                                      .AllowAnyOrigin());
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
