db605a2efecbc5f0216cfa3fb14aaf1b6e6c777a
Josué Diogo Pedro <pedrojosuediogo@gmail.com>
2024-04-09 03:45:30 -0700

    VShop.CartApi - implement GetCartByUserIdAsync on CartRepository

--- src/VShop.CartApi/Repositories/CartRepository.cs
+++ src/VShop.CartApi/Repositories/CartRepository.cs
@@ -1,15 +1,39 @@
-﻿using VShop.CartApi.Context;
+﻿using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using VShop.CartApi.Context;
 using VShop.CartApi.DTOs;
+using VShop.CartApi.Models;
 
 namespace VShop.CartApi.Repositories;
 
 public class CartRepository : ICartRepository
 {
     private readonly CartApiDbContext _context;
+    private readonly IMapper _mapper;
 
-    public CartRepository(CartApiDbContext context)
+    public CartRepository(CartApiDbContext context, IMapper mapper)
     {
         _context = context;
+        _mapper = mapper;
+    }
+    public async Task<CartDTO> GetCartByUserIdAsync(string userId)
+    {
+        Cart cart = new()
+        {
+            CartHeader = await _context.CartHeader
+                                                ?.AsNoTracking()
+                                                ?.DefaultIfEmpty()
+                                                ?.SingleOrDefaultAsync(header => header.UserId == userId)
+        };
+
+        cart.CartItems = await _context.CartItems?
+                                                .AsNoTracking()?
+                                                .DefaultIfEmpty()?
+                                                .Where(item => item.CartHeaderId == cart.CartHeader.CartHeaderId)
+                                                .Include(prod => prod.Product).ToListAsync();
+
+        return _mapper.Map<CartDTO>(cart);
+
     }
 
     public Task<bool> ApplyCouponAsync(string userId, string couponCode)
@@ -32,10 +56,6 @@ public class CartRepository : ICartRepository
         throw new NotImplementedException();
     }
 
-    public Task<CartDTO> GetCartByUserIdAsync(string userId)
-    {
-        throw new NotImplementedException();
-    }
 
     public Task<CartDTO> UpdateCartAsync(CartDTO cartDTO)
     {
