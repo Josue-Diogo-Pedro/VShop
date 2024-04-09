891400f74691c9a560af0a630067292a492748ce
Josué Diogo Pedro <pedrojosuediogo@gmail.com>
2024-04-09 03:02:50 -0700

    VShop.CartApi - add CartHeaderDTO

---
+++ src/VShop.CartApi/DTOs/CartHeaderDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace VShop.CartApi.DTOs;
+
+public class CartHeaderDTO
+{
+    public int CartHeaderId { get; set; }
+    public string UserId { get; set; } = string.Empty;
+    public string CouponCode { get; set; } = string.Empty;
+}
