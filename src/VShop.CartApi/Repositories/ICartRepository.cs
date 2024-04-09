c9852476bbb28592edd13d3f5f0d1c788fb7cda6
Josué Diogo Pedro <pedrojosuediogo@gmail.com>
2024-04-09 03:26:16 -0700

    VSop.CartApi - create ICartRepository

---
+++ src/VShop.CartApi/Repositories/ICartRepository.cs
@@ -0,0 +1,13 @@
+﻿using VShop.CartApi.DTOs;
+
+namespace VShop.CartApi.Repositories;
+
+public interface ICartRepository
+{
+    Task<CartDTO> GetCartByUserIdAsync(string userId);
+    Task<CartDTO> UpdateCartAsync(CartDTO cartDTO);
+    Task<bool> CleanCartAsync(string userId);
+    Task<bool> DeleteCartItemAsync(int cartItemId);
+
+    Task<bool> ApplyCouponAsync(string userId, string couponCode);
+}
