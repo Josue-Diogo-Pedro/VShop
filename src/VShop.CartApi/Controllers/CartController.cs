8862dbcd00d5c14dfefe17b2ecb13b15377b7749
Josué Diogo Pedro <pedrojosuediogo@gmail.com>
2024-04-14 16:47:45 -0700

    VShop.CartApi . create CartController and finish his implementation

---
+++ src/VShop.CartApi/Controllers/CartController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using VShop.CartApi.DTOs;
+using VShop.CartApi.Repositories;
+
+namespace VShop.CartApi.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class CartController : ControllerBase
+{
+    private readonly ICartRepository _cartRepository;
+
+	public CartController(ICartRepository cartRepository) => _cartRepository = cartRepository;
+
+    [HttpGet("getcart/{id}")]
+    public async Task<ActionResult<CartDTO>> GetCartByUserId(string userId)
+    {
+        var cartDto = await _cartRepository.GetCartByUserIdAsync(userId);
+
+        if (cartDto is null) return NotFound();
+
+        return Ok(cartDto);
+    }
+
+    [HttpPost("addcart")]
+    public async Task<ActionResult<CartDTO>> AddCart(CartDTO cartDTO)
+    {
+        var cart = await _cartRepository.UpdateCartAsync(cartDTO);
+
+        if (cart is null) return BadRequest();
+
+        return Ok(cart);
+    }
+
+    [HttpPut("updatecart")]
+    public async Task<ActionResult<CartDTO>> UpdateCart(CartDTO cartDTO)
+    {
+        var cart = await _cartRepository.UpdateCartAsync(cartDTO);
+
+        if(cart is null) return NotFound();
+
+        return Ok(cart);
+    }
+
+    [HttpDelete("deletecart/{id}")]
+    public async Task<ActionResult<bool>> DeleteCart(int id)
+    {
+        var status = await _cartRepository.DeleteCartItemAsync(id);
+
+        if (!status) return BadRequest();
+
+        return Ok(status);
+    }
+}
