8de0f5cd92a959eedda0a719ad429290b53e4460
Josué Diogo Pedro <pedrojosuediogo@gmail.com>
2024-04-14 16:58:23 -0700

    VShop.CartApi - fix CartItems removing CartHeader and express default quantity as 1

--- src/VShop.CartApi/DTOs/CartItemDTO.cs
+++ src/VShop.CartApi/DTOs/CartItemDTO.cs
@@ -5,9 +5,9 @@ namespace VShop.CartApi.DTOs;
 public class CartItemDTO
 {
     public int CartItemId { get; set; }
-    public int Quantity { get; set; }
+    public int Quantity { get; set; } = 1;
     public int ProductId { get; set; }
     public int CartHeaderId { get; set; }
     public ProductDTO Product { get; set; } = new();
-    public CartHeaderDTO CartHeader { get; set; } = new();
+    //public CartHeaderDTO CartHeader { get; set; } = new();
 }
--- src/VShop.CartApi/Models/CartItem.cs
+++ src/VShop.CartApi/Models/CartItem.cs
@@ -3,9 +3,9 @@
 public class CartItem
 {
     public int CartItemId { get; set; }
-    public int Quantity { get; set; }
+    public int Quantity { get; set; } = 1;
     public int ProductId { get; set; }
     public int CartHeaderId { get; set; }
     public Product Product { get; set; } = new();
-    public CartHeader CartHeader { get; set; } = new();
+    //public CartHeader CartHeader { get; set; } = new();
 }
