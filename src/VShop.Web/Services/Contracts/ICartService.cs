e873c9ca94b8c2558c31c9415ea1f2dba53ddb32
Josué Diogo Pedro <pedrojosuediogo@gmail.com>
2024-04-14 17:19:10 -0700

    VShop.Web - create interface for ICartService

---
+++ src/VShop.Web/Services/Contracts/ICartService.cs
@@ -0,0 +1,19 @@
+﻿using VShop.Web.Models;
+
+namespace VShop.Web.Services.Contracts;
+
+public interface ICartService
+{
+    Task<CartViewModel> GetCartByUserIdAsync(string userId, string token);
+    Task<CartViewModel> AddItemToCartAsync(CartViewModel cartVM, string token);
+    Task<CartViewModel> UpdateCartAsync(CartViewModel cartVM, string token);
+    Task<bool> RemoveItemFromCartAsync(int cartId, string token);
+
+    //Implementation of Report
+    Task<bool> ApplyCouponAsync(CartViewModel cartVM, string couponCode, string token);
+    Task<bool> RemoveCouponAsync(string userId, string token);
+    Task<bool> ClearCartAsync(string userId, string token);
+
+    Task<CartViewModel> Chekout(CartHeaderViewModel cartHeader, string token);
+
+}
