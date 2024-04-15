d0def213bad3ac3a4d50e51580543822c3dc3cdd
Josué Diogo Pedro <pedrojosuediogo@gmail.com>
2024-04-14 17:23:59 -0700

    VShop.Web - create CartService and begin to implement

---
+++ src/VShop.Web/Services/CartService.cs
@@ -0,0 +1,20 @@
+﻿using System.Text.Json;
+using VShop.Web.Models;
+
+namespace VShop.Web.Services;
+
+public class CartService
+{
+    private readonly IHttpClientFactory _httpClientFactory;
+    private readonly JsonSerializerOptions? _optins;
+    private string apiEndpoint = "/api/cart";
+    private CartViewModel cartViewModel = new();
+
+    public CartService(IHttpClientFactory httpClientFactory)
+    {
+        _httpClientFactory = httpClientFactory;
+        _optins = new JsonSerializerOptions { PropertyNameCaseInsensitive = true };
+    }
+
+
+}
