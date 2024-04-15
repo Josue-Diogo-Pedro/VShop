9bc5ee4cf05c9b545ead954ab85be87adeaa00eb
Josué Diogo Pedro <pedrojosuediogo@gmail.com>
2024-04-14 17:07:25 -0700

    VShop.Web - create CartViewModel

---
+++ src/VShop.Web/Models/CartViewModel.cs
@@ -0,0 +1,7 @@
+﻿namespace VShop.Web.Models;
+
+public class CartViewModel
+{
+    public CartHeaderViewModel CartHeader { get; set; } = new();
+    public IEnumerable<CartItemViewModel> CartItems { get; set; } = new List<CartItemViewModel>();
+}
