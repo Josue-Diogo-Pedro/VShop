ab93a4c28c007784f69783cbf1062450a37f2ea9
Josué Diogo Pedro <pedrojosuediogo@gmail.com>
2024-04-16 14:34:30 -0700

    VShop.Web - create method to access_token and GetUserId

--- src/VShop.Web/Controllers/CartController.cs
+++ src/VShop.Web/Controllers/CartController.cs
@@ -1,11 +1,27 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using VShop.Web.Services.Contracts;
 
 namespace VShop.Web.Controllers;
 
 public class CartController : Controller
 {
+    private readonly ICartService _cartService;
+
+    public CartController(ICartService cartService) => _cartService = cartService;
+
+    [Authorize]
     public IActionResult Index()
     {
         return View();
     }
+
+    #region Private Functions
+
+    private async Task<string> GetAccessToken() => await HttpContext.GetTokenAsync("access_token");
+
+    private string GetUserId() => User.Claims?.Where(user => user.Type == "sub")?.FirstOrDefault()?.Value;
+
+    #endregion
 }
