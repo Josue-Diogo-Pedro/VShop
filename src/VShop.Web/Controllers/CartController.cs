851672f912fb473099530f0bce52a266c6265178
Josué Diogo Pedro <pedrojosuediogo@gmail.com>
2024-04-16 14:28:32 -0700

    VShop.Web - add CartController and begin his implementation

---
+++ src/VShop.Web/Controllers/CartController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace VShop.Web.Controllers;
+
+public class CartController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
