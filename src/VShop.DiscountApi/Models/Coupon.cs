843f8f179e0043c504f6961d16567a23861959ed
Josué Diogo Pedro <pedrojosuediogo@gmail.com>
2024-04-24 16:53:25 -0700

    VShop.Discount - add Coupon model

---
+++ src/VShop.DiscountApi/Models/Coupon.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace VShop.DiscountApi.Models;
+
+public class Coupon
+{
+    [Required]
+    [StringLength(30)]
+    public int CouponId { get; set; }
+    
+    public string? CouponCode { get; set; }
+
+    [Required]
+    public decimal Discount { get; set; }
+}
