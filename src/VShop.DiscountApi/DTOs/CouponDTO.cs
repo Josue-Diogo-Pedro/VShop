df9a96b71392a359dbc4d3b890b194a033f6cbd6
Josué Diogo Pedro <pedrojosuediogo@gmail.com>
2024-04-24 16:59:00 -0700

    VShop.DiscountApi - add CouponDTO model

---
+++ src/VShop.DiscountApi/DTOs/CouponDTO.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace VShop.DiscountApi.DTOs;
+
+public class CouponDTO
+{
+    public int CouponId { get; set; }
+    public string? CouponCode { get; set; }
+    public decimal Discount { get; set; }
+}
