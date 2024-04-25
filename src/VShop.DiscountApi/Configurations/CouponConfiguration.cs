874a785b9981b1ab35913bf3f20ff082e00631d0
Josué Diogo Pedro <pedrojosuediogo@gmail.com>
2024-04-24 17:19:42 -0700

    VShop.DiscountApi - populate initial data

--- src/VShop.DiscountApi/Configurations/CouponConfiguration.cs
+++ src/VShop.DiscountApi/Configurations/CouponConfiguration.cs
@@ -15,5 +15,19 @@ public class CouponConfiguration : IEntityTypeConfiguration<Coupon>
         builder.Property(p => p.Discount).HasColumnType("decimal(10,2)");
 
         builder.HasIndex(p => p.CouponCode).IsUnique();
+
+        builder.HasData(new Coupon
+        {
+            CouponId = 1,
+            CouponCode = "VSHOP_PROMO_10",
+            Discount = 10
+        });
+
+        builder.HasData(new Coupon
+        {
+            CouponId = 2,
+            CouponCode = "VSHOP_PROMO_20",
+            Discount = 20
+        });
     }
 }
