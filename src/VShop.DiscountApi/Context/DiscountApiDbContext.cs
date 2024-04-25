772426af9edb5aac8d9e6e726b9bc313725a43b9
Josué Diogo Pedro <pedrojosuediogo@gmail.com>
2024-04-24 17:42:29 -0700

    VShop.DiscountApi - creating DiscountApiDbContext to manage app with database

---
+++ src/VShop.DiscountApi/Context/DiscountApiDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using VShop.DiscountApi.Models;
+
+namespace VShop.DiscountApi.Context;
+
+public class DiscountApiDbContext : DbContext
+{
+    public DbSet<Coupon>? Coupons { get; set; }
+
+	public DiscountApiDbContext(DbContextOptions<DiscountApiDbContext> options) : base(options) { }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(DiscountApiDbContext).Assembly);
+    }
+}
