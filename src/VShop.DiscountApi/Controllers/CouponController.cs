694c66918ffbc0f925cb2e47649129bbbdc5b0bf
Josué Diogo Pedro <pedrojosuediogo@gmail.com>
2024-04-24 18:56:23 -0700

    VShop.DiscountApi - creating CouponController

---
+++ src/VShop.DiscountApi/Controllers/CouponController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using VShop.DiscountApi.DTOs;
+using VShop.DiscountApi.Repositories;
+
+namespace VShop.DiscountApi.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class CouponController : ControllerBase
+{
+    private readonly ICouponRepository _couponRepository;
+
+	public CouponController(ICouponRepository couponRepository) => _couponRepository = couponRepository ?? 
+                                                                                        throw new ArgumentNullException(nameof(couponRepository));
+
+    public async Task<ActionResult<CouponDTO>> GetDiscountCouponByCode(string couponCode)
+    {
+        var coupon = await _couponRepository.GetCouponByCode(couponCode);
+
+        if (coupon is null) return NotFound($"Coupon code: {couponCode} not found");
+
+        return Ok(coupon);
+    }
+}
