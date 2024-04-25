58e393adc04fae9f26da1c252603db1ebe132f1b
Josué Diogo Pedro <pedrojosuediogo@gmail.com>
2024-04-24 18:47:43 -0700

    VShop.DiscountApi - migration initial

---
+++ src/VShop.DiscountApi/Migrations/20240425014505_Initial.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace VShop.DiscountApi.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Coupons",
+                columns: table => new
+                {
+                    CouponId = table.Column<int>(type: "int", maxLength: 30, nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CouponCode = table.Column<string>(type: "varchar(30)", nullable: true),
+                    Discount = table.Column<decimal>(type: "decimal(10,2)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Coupons", x => x.CouponId);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Coupons",
+                columns: new[] { "CouponId", "CouponCode", "Discount" },
+                values: new object[] { 1, "VSHOP_PROMO_10", 10m });
+
+            migrationBuilder.InsertData(
+                table: "Coupons",
+                columns: new[] { "CouponId", "CouponCode", "Discount" },
+                values: new object[] { 2, "VSHOP_PROMO_20", 20m });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Coupons_CouponCode",
+                table: "Coupons",
+                column: "CouponCode",
+                unique: true,
+                filter: "[CouponCode] IS NOT NULL");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Coupons");
+        }
+    }
+}
