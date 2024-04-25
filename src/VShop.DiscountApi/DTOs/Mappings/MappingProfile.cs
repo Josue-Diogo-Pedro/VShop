68e6eca4c4132a785b064728435e97f5c4fc1e62
Josué Diogo Pedro <pedrojosuediogo@gmail.com>
2024-04-24 17:03:38 -0700

    VShop.DiscountApi - create MappingProfile to use DTO in place of original model

---
+++ src/VShop.DiscountApi/DTOs/Mappings/MappingProfile.cs
@@ -0,0 +1,12 @@
+﻿using AutoMapper;
+using VShop.DiscountApi.Models;
+
+namespace VShop.DiscountApi.DTOs.Mappings;
+
+public class MappingProfile : Profile
+{
+	public MappingProfile()
+	{
+		CreateMap<Coupon, CouponDTO>().ReverseMap();
+	}
+}
