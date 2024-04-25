cf8c9adedbcfec3ff4afcaa2ce940d17399b36b4
Josué Diogo Pedro <pedrojosuediogo@gmail.com>
2024-04-24 17:56:15 -0700

    VShop.DiscountApi - creating implementation for ICouponRepository in CouponRepository

---
+++ src/VShop.DiscountApi/Repositories/CouponRepository.cs
@@ -0,0 +1,25 @@
+﻿using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using VShop.DiscountApi.Context;
+using VShop.DiscountApi.DTOs;
+
+namespace VShop.DiscountApi.Repositories;
+
+public class CouponRepository : ICouponRepository
+{
+    private readonly DiscountApiDbContext _context;
+    private readonly IMapper _mapper;
+
+    public CouponRepository(DiscountApiDbContext context, IMapper mapper)
+    {
+        _context = context;
+        _mapper = mapper;
+    }
+
+    public async Task<CouponDTO> GetCouponByCode(string code)
+    {
+        var coupon = await _context.Coupons?.SingleOrDefaultAsync(c => c.CouponCode == code);
+
+        return _mapper.Map<CouponDTO>(coupon);
+    }
+}
