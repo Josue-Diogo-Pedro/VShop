4d46ad2cb4a741bdb423198b4824b8fd40f9a7b3
Josué Diogo Pedro <pedrojosuediogo@gmail.com>
2024-04-27 13:24:09 -0700

    VShop.Web - update CartHeaderViewModel with more properties

--- src/VShop.Web/Models/CartHeaderViewModel.cs
+++ src/VShop.Web/Models/CartHeaderViewModel.cs
@@ -10,4 +10,17 @@ public class CartHeaderViewModel
 
     //Receive discount
     public decimal Discount { get; set; } = 0.00m;
+
+    //checkout
+    public string FirstName { get; set; } = string.Empty;
+    public string LastName { get; set; } = string.Empty;
+    public DateTime DateTime { get; set; }
+    public string Telephone { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+
+    //Credit card
+    public string CardNumber { get; set; } = string.Empty;
+    public string NameOnCard { get; set; } = string.Empty;
+    public string CVV { get; set; } = string.Empty;
+    public string ExpireMonthYear { get; set; } = string.Empty;
 }
