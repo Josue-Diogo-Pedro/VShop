5a6c7648bb5e6d45b087e27fe1e8a42b3ea80cd6
Josué Diogo Pedro <pedrojosuediogo@gmail.com>
2024-04-27 13:42:41 -0700

    VShop.CartApi - create model for CheckoutHeaderDTO

---
+++ src/VShop.CartApi/DTOs/CheckoutHeaderDTO.cs
@@ -0,0 +1,29 @@
+﻿namespace VShop.CartApi.DTOs;
+
+public class CheckoutHeaderDTO
+{
+    public int Id { get; set; }
+    public string UserId { get; set; } = string.Empty;
+    public string CouponCode { get; set; } = string.Empty;
+
+    public decimal TotalAmount { get; set; } = 0.00m;
+
+    //desconto
+    public decimal Discount { get; set; } = 0.00m;
+
+    //checkout
+    public string FirstName { get; set; } = string.Empty;
+    public string LastName { get; set; } = string.Empty;
+    public DateTime DateTime { get; set; }
+    public string Telephone { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+
+    //Credit card
+    public string CardNumber { get; set; } = string.Empty;
+    public string NameOnCard { get; set; } = string.Empty;
+    public string CVV { get; set; } = string.Empty;
+    public string ExpireMothYear { get; set; } = string.Empty;
+    //
+    public int CartTotalItens { get; set; }
+    public IEnumerable<CartItemDTO>? CartItems { get; set; }
+}
